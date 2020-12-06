2ce878b1304f3d0705328c58a11f1eb0cbd419df
PC <PC@DESKTOP-3K4JC73>
2020-12-06 12:57:20 +0330

    register consumers in teacher's dependencies

--- Teacher/Sofa.Teacher.DependencyInjection/RegisterServiceBus.cs
+++ Teacher/Sofa.Teacher.DependencyInjection/RegisterServiceBus.cs
@@ -5,6 +5,27 @@ using Sofa.Teacher.Consumer.LessonPlanConsumer.RegisterLessonPlan;
 using Sofa.Teacher.Consumer.UserConsumer.RegisterUser;
 using Sofa.Teacher.Consumer.ReisterPost;
 using Sofa.Teacher.Repository;
+using Sofa.Teacher.Consumer.UserConsumer.UpdateUserIsActiveStatus;
+using Sofa.Teacher.Consumer.UserConsumer.UpdateUserIsDeletedStatus;
+using Sofa.Teacher.Consumer.CourseConsumer.RegisterCourse;
+using Sofa.Teacher.Consumer.CourseConsumer.UpdateCourseIsActiveStatus;
+using Sofa.Teacher.Consumer.CourseConsumer.UpdateCourseIsDeletedStatus;
+using Sofa.Teacher.Consumer.FieldConsumer.RegisterField;
+using Sofa.Teacher.Consumer.FieldConsumer.UpdateFieldIsActiveStatus;
+using Sofa.Teacher.Consumer.FieldConsumer.UpdateFieldIsDeletedStatus;
+using Sofa.Teacher.Consumer.InstituteConsumer.RegisterInstitute;
+using Sofa.Teacher.Consumer.InstituteConsumer.UpdateInstituteIsActiveStatus;
+using Sofa.Teacher.Consumer.InstituteConsumer.UpdateInstituteIsDeletedStatus;
+using Sofa.Teacher.Consumer.LessonPlanConsumer.UpdateLessonPlanIsActiveStatus;
+using Sofa.Teacher.Consumer.LessonPlanConsumer.UpdateLessonPlanIsDeletedStatus;
+using Sofa.Teacher.Consumer.PostConsumer.UpdatePostIsDeletedStatus;
+using Sofa.Teacher.Consumer.PostConsumer.UpdatePostIsActiveStatus;
+using Sofa.Teacher.Consumer.SessionConsumer.RegisterSession;
+using Sofa.Teacher.Consumer.SessionConsumer.UpdateSessionIsActiveStatus;
+using Sofa.Teacher.Consumer.SessionConsumer.UpdateSessionIsDeletedStatus;
+using Sofa.Teacher.Consumer.TermConsumer.RegisterTerm;
+using Sofa.Teacher.Consumer.TermConsumer.UpdateTermIsActiveStatus;
+using Sofa.Teacher.Consumer.TermConsumer.UpdateTermIsDeletedStatus;
 
 namespace Sofa.Teacher.DependencyResolver
 {
@@ -26,9 +47,46 @@ namespace Sofa.Teacher.DependencyResolver
 
                     cfg.ReceiveEndpoint(busSettings.QueueName, x =>
                     {
-                        x.Instance(new RegisterUserEventConsumer(context.Resolve<IUnitOfWork>(), context.Resolve<ILogger>()));
-                        x.Instance(new RegisterLessonPlanEventConsumer(context.Resolve<IUnitOfWork>(), context.Resolve<ILogger>()));
-                        x.Instance(new RegisterPostEventConsumer(context.Resolve<IUnitOfWork>(), context.Resolve<ILogger>()));
+                        #region Course Consumers
+                            x.Instance(new RegisterCourseEventConsumer(context.Resolve<IUnitOfWork>(), context.Resolve<ILogger>()));
+                            x.Instance(new UpdateCourseIsActiveStatusEventConsumer(context.Resolve<IUnitOfWork>(), context.Resolve<ILogger>()));
+                            x.Instance(new UpdateCourseIsDeletedStatusEventConsumer(context.Resolve<IUnitOfWork>(), context.Resolve<ILogger>()));
+                        #endregion
+                        #region Field Consumers
+                            x.Instance(new RegisterFieldEventConsumer(context.Resolve<IUnitOfWork>(), context.Resolve<ILogger>()));
+                            x.Instance(new UpdateFieldIsActiveStatusEventConsumer(context.Resolve<IUnitOfWork>(), context.Resolve<ILogger>()));
+                            x.Instance(new UpdateFieldIsDeletedStatusEventConsumer(context.Resolve<IUnitOfWork>(), context.Resolve<ILogger>()));
+                        #endregion
+                        #region Institute Consumers
+                            x.Instance(new RegisterInstituteEventConsumer(context.Resolve<IUnitOfWork>(), context.Resolve<ILogger>()));
+                            x.Instance(new UpdateInstituteIsActiveStatusEventConsumer(context.Resolve<IUnitOfWork>(), context.Resolve<ILogger>()));
+                            x.Instance(new UpdateInstituteIsDeletedStatusEventConsumer(context.Resolve<IUnitOfWork>(), context.Resolve<ILogger>()));
+                        #endregion
+                        #region LessonPlan Consumers
+                            x.Instance(new RegisterLessonPlanEventConsumer(context.Resolve<IUnitOfWork>(), context.Resolve<ILogger>()));
+                            x.Instance(new UpdateLessonPlanIsActiveStatusEventConsumer(context.Resolve<IUnitOfWork>(), context.Resolve<ILogger>()));
+                            x.Instance(new UpdateLessonPlanIsDeletedStatusEventConsumer(context.Resolve<IUnitOfWork>(), context.Resolve<ILogger>()));
+                        #endregion
+                        #region Post Consumers
+                            x.Instance(new RegisterPostEventConsumer(context.Resolve<IUnitOfWork>(), context.Resolve<ILogger>()));
+                            x.Instance(new UpdatePostIsActiveStatusEventConsumer(context.Resolve<IUnitOfWork>(), context.Resolve<ILogger>()));
+                            x.Instance(new UpdatePostIsDeletedStatusEventConsumer(context.Resolve<IUnitOfWork>(), context.Resolve<ILogger>()));
+                        #endregion
+                        #region Session Consumers
+                            x.Instance(new RegisterSessionEventConsumer(context.Resolve<IUnitOfWork>(), context.Resolve<ILogger>()));
+                            x.Instance(new UpdateSessionIsActiveStatusEventConsumer(context.Resolve<IUnitOfWork>(), context.Resolve<ILogger>()));
+                            x.Instance(new UpdateSessionIsDeletedStatusEventConsumer(context.Resolve<IUnitOfWork>(), context.Resolve<ILogger>()));
+                        #endregion
+                        #region Term Consumers
+                            x.Instance(new RegisterTermEventConsumer(context.Resolve<IUnitOfWork>(), context.Resolve<ILogger>()));
+                            x.Instance(new UpdateTermIsActiveStatusEventConsumer(context.Resolve<IUnitOfWork>(), context.Resolve<ILogger>()));
+                            x.Instance(new UpdateTermIsDeletedStatusEventConsumer(context.Resolve<IUnitOfWork>(), context.Resolve<ILogger>()));
+                        #endregion
+                        #region User Consumers
+                            x.Instance(new RegisterUserEventConsumer(context.Resolve<IUnitOfWork>(), context.Resolve<ILogger>()));
+                            x.Instance(new UpdateUserIsActiveStatusEventConsumer(context.Resolve<IUnitOfWork>(), context.Resolve<ILogger>()));
+                            x.Instance(new UpdateUserIsDeletedStatusEventConsumer(context.Resolve<IUnitOfWork>(), context.Resolve<ILogger>()));
+                        #endregion
                     });
                 });
                 return busControl;
